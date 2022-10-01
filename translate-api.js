62391633dbf461f02a71f016e23f5d11b2eba444
syb <903923381@qq.com>
2022-10-01 17:45:56 +0800

    feat: 翻译功能实现

---
+++ translate-api.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+const vscode = require('vscode')
+const md5 = require('md5')
+const appid = vscode.workspace.getConfiguration().get('translate.appid')
+const secret = vscode.workspace.getConfiguration().get('translate.secret')
+
+module.exports = {
+    /**
+     * 翻译方法
+     * @param {string} q 查询字符串
+     * @param {string} from 源语言
+     * @param {string} to 目标语言
+    * @returns {
+      Promise<{
+      data: {
+      trans_result: [{
+        src: string, dst: string
+      }]
+    }
+  }>} Promise翻译结果
+     */
+    trnaslate(q, from, to) {
+        let salt = Math.random()
+        return axios({
+            method: 'get',
+            url: 'https://fanyi-api.baidu.com/api/trans/vip/translate',
+            params: {
+                q,
+                appid,
+                from,
+                to,
+                salt,
+                sign: md5(appid + q + salt + secret)
+            }
+        })
+    }
+}
